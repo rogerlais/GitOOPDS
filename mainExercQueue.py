335c95057930508306a33f68533e6bd2cf5bef44
Roger <rogerlais@gmail.com>
2019-09-27 00:57:33 -0300

    user interface preview

--- mainExercQueue.py
+++ mainExercQueue.py
@@ -1,6 +1,12 @@
-#import sys
+import curses
+import os
+import sys
+import colorama
+import builtins
+import time
 #sys.path.insert(0, './GitOOPDS/QueueExerc')
 import QueueExerc.bank_classes  #import bank_classes
+import msvcrt
 from datetime import datetime, timedelta
 
 def init_simulation():
@@ -15,6 +21,15 @@ def generate_report(incoming, outcoming, agency):
     print( f'Neste ciclo chegaram { agency.get_avg_wait_time() } clientes')
     return
 
+def get_user_continue():
+    print( 'Deseja repetir a simulação?(S/N): ', end='')
+    key = bytes( 'Z', 'utf-8' )
+    while( not( key.decode().upper() in ['S', 'N'] )):
+       if msvcrt.kbhit():
+           key = msvcrt.getch()
+           print(key.decode(), end='')   # just to show the result
+    return ( key.decode().upper() == 'S' )
+
 
 def main():
     agency = init_simulation()
@@ -25,9 +40,12 @@ def main():
         incoming = agency.incoming_clients()
         outcoming = agency.outcoming_clients()
         generate_report(incoming, outcoming, agency )
-        repeat_step = input( 'Deseja repetir a simulação?(S/N): ').upper() == 'S'
+        time.sleep(1)
+        repeat_step = get_user_continue()
     return
 
 
 ######################### PONTO DE ENTRADA  #########################
+#value = int(raw_input("Enter the inputs : ") or "42")
+
 main()
