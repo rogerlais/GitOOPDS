42c1c754ba265af206eb189d3e4daf1a0df93688
Roger <rogerlais@gmail.com>
2019-10-04 00:59:43 -0300

    code docs comments final logic build need better presentation

--- QueueExerc/bank_classes.py
+++ QueueExerc/bank_classes.py
@@ -6,61 +6,90 @@ from base_classes.queue import QueueP2
 
 
 class Client(object):
-    def __init__(self, accept_time ):
+    """Classe para abstração de cliente do banco"""
+
+    def __init__(self, accept_time):
+        """Construtor recebe a marca de tempo de entrada do cliente"""
         self._time_mark = accept_time
         return
 
     @property
     def time_mark(self):
+        """Marca de tempo da entrada deste cliente na agência"""
         return self._time_mark
 
     def get_wait_time(self, local_time):
-        return (local_time - self._time_mark)
+        """Tempo de espera na fila deste cliente em segundos"""
+        return (local_time - self._time_mark).seconds
 
 ####################### Class CashTerminal #################################
 
 
 class CashTerminal(QueueP2):
+    """Classe da abstração terminal de autoatendimento"""
 
-    def __init__(self, label):
+    def __init__(self, parent, label):
+        """Construtor recebe rótulo deste terminal(sem validação de unicidade)"""
         super().__init__()
         self._label = label
+        self._parent = parent
         return
 
     @property
     def label(self):
+        """Rótulo do terminal"""
         return self._label
 
     @property
-    def client_count(self):
-        return self.count
+    def avg_wait_time(self):
+        if( self.count > 0 ):
+            sum = 0
+            for c in self.queue:
+                sum += c.get_wait_time( self._parent.local_time )
+            return ( sum / self.count )
+        else:
+            return 0
+
+    @property
+    def max_wait_time(self):
+        ret = 0
+        for c in self.queue:
+            if( c.get_wait_time( self._parent.local_time ) > ret ):
+                ret = c.get_wait_time( self._parent.local_time )
+        return ret
 
     def get_max_wait_time(self, current_time):
-        ret = timedelta( 0 )
+        """Tempo máximo de espera dos clientes neste terminal em segundos"""
+        ret = 0
         for client in self.queue:
-            if( ret < client.get_wait_time( current_time ) ):
-                ret = client.get_wait_time( current_time )
+            if(ret < client.get_wait_time(current_time)):
+                ret = client.get_wait_time(current_time)
         return ret
 
     def incoming_client(self, client):
+        """Aceita cliente na fila deste terminal. Retorna a quantidade de clientes atualmente na fila"""
         self.insert_data(client)
         return len(self.queue)
 
     # Recebe a quantidade de clientes a serem processados e retorna a quantidade efetivamente processada
     def process_clients(self, count):
+        """Evento de processamento dos clientes na fila. Extrai a quantidade informada como argumento"""
         ret = 0
         for _ in range(count):
-            ret += self.remove_data()
+            if( self.remove_data() is not None ):
+                ret += 1
         return ret
 
     def remove_data(self):
+        """Remove o primeiro cliente da fila, retornando o mesmo ou None caso fila vazia"""
         if(self.count > 0):
-            super().remove_data()
-            return 1
-        return 0
+            return super().remove_data()
+        else:
+            return None
 
     def get_total_queue_time(self, local_time):
-        ret = timedelta(0)
+        """Tempo total de espera de todos os clientes em segundos"""
+        ret = 0
         for client in self.queue:
             ret += client.get_wait_time(local_time)
         return ret
@@ -69,117 +98,155 @@ class CashTerminal(QueueP2):
 
 
 class BankAgency(object):
+    """Abstração de agência bancária"""
 
     def __init__(self, terminal_count, time):
+        """Construtor da agência.
+        terminal_count -> Quantidade de terminais existentes
+        time -> Marca de tempo atual
+        """
         super().__init__()
         self._terminals = []
         self._local_time = time
         for term_count in range(terminal_count):
-            self._terminals.append(CashTerminal(term_count))
+            self._terminals.append(CashTerminal(self, term_count))
         return
 
     @property
+    def terminals(self):
+        """Lista de terminais presentes na agência"""
+        return self._terminals
+
+    @property
     def local_time(self):
+        """Marca de tempo "ATUAL" da simulação"""
         return self._local_time
 
     @property
     def client_count(self):
+        """Quantidade total de clientes presentes na agência"""
         ret = 0
         for term in self._terminals:
             ret += term.count
         return ret
-    
+
     @property
     def avg_wait_time(self):
-        total_time = timedelta(0)
+        """Tempo médio(segundos) de espera de todos os clientes presentes"""
+        total_time = 0
         client_count = 0
         for terminal in self._terminals:
             total_time += terminal.get_total_queue_time(self._local_time)
             client_count += terminal.count
-        if(client_count > 0):
-            #! validar o resultado
-            return total_time.seconds/client_count
+        if(client_count > 0):  # validar o resultado
+            return total_time/client_count
         else:
             return 0
 
     @property
     def max_wait_time(self):
-        ret = timedelta(0)
+        """Tempo máximo de espera do cliente mais azarado"""
+        ret = 0
         for term in self._terminals:
-            if( ret < term.get_max_wait_time( self._local_time ) ):
+            if(ret < term.get_max_wait_time(self._local_time)):
                 ret = term.max_wait_time
         return ret
 
     def increment_time(self, delta):
+        """Incrementa o tempo simulado pela quantidade de minutos informada como argumento"""
         minutes = timedelta(0, 0, 0, 0, delta)
         self._local_time = self._local_time + minutes
         return self._local_time
 
     def accept_client(self):
+        """Registra a entrada de novo cliente e redireciona ao terminal de menor fila"""
         cash_terminal = self.prior_terminal
-        client = Client( self.local_time )
+        client = Client(self.local_time)
         cash_terminal.incoming_client(client)
-        return
+        return client
 
     @property
     def prior_terminal(self):
-        # Varre lista de caixas para encontrar o com menos clientes
+        """Varre lista de caixas e retorna qual terminal tem menos clientes no momento"""
         ret = self._terminals[0]
         for terminal in self._terminals:
-            if terminal.client_count < ret.client_count:  # Terminal com menos clientes dentre os existentes
+            if terminal.count < ret.count:  # Terminal com menos clientes dentre os existentes
                 ret = terminal
         return ret
 
-    def outcoming_clients(self):
+    def outcoming_clients(self, max_client_count):
+        """Dispara e processa o evento de atendimento para todos os terminais. 
+        Retorna a quantidade total de clentes atendidos neste ciclo.
+        max_client_count -> Máximo de clientes atentidos por terminal em cada ciclo
+        """
         out = 0
         for c in self._terminals:
             # remover clientes entre 1 e 2
-            out += c.process_clients(random.randint(1, 2))
+            out += c.process_clients(random.randint(1, max_client_count))
         return out
 
-    def incoming_clients(self):
-        # Processa as entradas
-        incoming_count = random.randint(4, 16)
+    def incoming_clients(self, min_client_count, max_client_count):
+        """Dispara e processa o evento de entrada de clientes na agência.
+        min_client_count -> mínimo de clientes por ciclo
+        max_client_count -> máximo de clientes por ciclo
+        Retorna a quantidade total de clientes entrantes neste ciclo
+        """
+        incoming_count = random.randint(min_client_count, max_client_count)
         for cc in range(incoming_count):
             self.accept_client()
-        # Processa as saí­das
-        self.outcoming_clients()
         return incoming_count
 
 
 ############### Class SimMediator  ######################
 
 class SimMediator(object):
-    def __init__(self, terminal_count):
+    """Classe de controle da simulação"""
+
+    def __init__(self, terminal_count, min_clients_per_cycle, max_clients_per_cycle, max_clients_per_terminal):
+        """Instancia um controlador da simulação e demais elementos internos
+        min_clients_per_cycle: Mínimo clientes entrantes por ciclo
+        max_clients_per_cycle: Máximo clientes entrantes por ciclo
+        max_clients_per_terminal: Mínimo clientes saintes por ciclo para cada terminal
+        """
         super().__init__()
         self._local_time = datetime.now()
         self._agency = BankAgency(terminal_count, self._local_time)
+        self._min_clients_per_cycle = min_clients_per_cycle
+        self._max_clients_per_cycle = max_clients_per_cycle
+        self._max_clients_per_terminal = max_clients_per_terminal
         self._cycle_count = 0
         self._cycle_incoming = 0
         self._cycle_outcoming = 0
 
     @property
     def cycle_incoming(self):
+        """Quantidade de clientes entrantes no ciclo atual da simulação"""
         return self._cycle_incoming
 
     @property
     def cycle_outcoming(self):
+        """Quantidade de clientes saintes no ciclo atual da simulação"""
         return self._cycle_outcoming
 
     @property
     def cycle_count(self):
+        """Contador de ciclos executados"""
         return self._cycle_count
 
     @property
     def local_time(self):
+        """Marca de tempo simulado(diferente do tempo real)"""
         return self._local_time
 
     @property
     def agency(self):
+        """Agência da simulação"""
         return self._agency
 
     def do_cycle(self):
+        """Dispara e processa o evento de ciclo de simulação, incrementando o tempo em 1 minuto"""
+        self._cycle_count += 1  #Contador de ciclos
         self._agency.increment_time(1)  # tempo em minutos
-        self._cycle_incoming = self._agency.incoming_clients()
-        self._cycle_outcoming = self._agency.outcoming_clients()
+        self._cycle_incoming = self._agency.incoming_clients(self._min_clients_per_cycle, self._max_clients_per_cycle )
+        self._cycle_outcoming = self._agency.outcoming_clients( self._max_clients_per_terminal )
         return
--- mainExercQueue.py
+++ mainExercQueue.py
@@ -16,6 +16,24 @@ def init_simulation():
     return agency
 
 
+def printMatrix(s):
+    # Do heading
+    print("     ", end="")
+    for j in range(len(s[0])):
+        print("%5s " % j, end="")
+    print()
+    print("     ", end="")
+    for j in range(len(s[0])):
+        print("------", end="")
+    print()
+    # Matrix contents
+    for i in range(len(s)):
+        print("%3s |" % (i), end="")  # Row nums
+        for j in range(len(s[0])):
+            print("%5s " % (s[i][j]), end="")
+        print()
+
+
 def show_report(control):
     print('')
     print(f'Hora simulada:{ control.local_time }       Ciclo#: {control.cycle_count}')
@@ -23,11 +41,17 @@ def show_report(control):
     print(f'Neste ciclo sairam  : { control.cycle_outcoming } clientes')
     print(f'Existem { control.agency.client_count } clientes na agência')
     print('TEMPOS:')
-    avg_time = control.agency.avg_wait_time.total_seconds()
+    avg_time = control.agency.avg_wait_time
     str_avg = f'{ avg_time // 60 } minutos e { avg_time % 60 } segundos'
-    max_time = control.agency.max_wait_time.total_seconds()
+    max_time = control.agency.max_wait_time
     str_max = f'{ max_time // 60 } minutos e { max_time % 60 } segundos'
-    print( f'Tempo médio: { str_avg }    Tempo máximo: { str_max } ')
+    print(f'Tempo médio: { str_avg }    Tempo máximo: { str_max } ')
+    report_array = []
+    report_array.append(['Caixa', 'Clientes', 'Média', 'Máximo'])
+    for term in control.agency.terminals:
+        report_array.append([term.label, term.count, term.avg_wait_time, term.max_wait_time])
+    #print('\n'.join([str(n) + ":  " + str(entry) for (n, entry) in zip(range(0, len(report_array)), report_array)]))
+    printMatrix( report_array )
     return
 
 
@@ -42,7 +66,7 @@ def get_user_continue():
 
 
 def main():
-    simulator = SimMediator(5)
+    simulator = SimMediator(5, 4, 16, 2)
     # Inicia simulação temporal por timeslice de 1 minuto, onde podem entrar de 4 a 16 clientes e sair de 1 a 2 clientes pode caixa
     repeat_step = True
     while(repeat_step):
@@ -50,7 +74,9 @@ def main():
         show_report(simulator)
         time.sleep(1)
         repeat_step = get_user_continue()
-    return
+    print('SIMULAÇÃO FINALIZADA PELO OPERADOR!')
+    return    
+
 
 
 ######################### PONTO DE ENTRADA  #########################
