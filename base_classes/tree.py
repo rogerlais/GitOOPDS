8134fa37ebf8e71874eef14565f38bce82e18f2a
rogerlais <rogerlais@gmail.com>
2019-10-25 09:25:45 -0300

    aula hoje

--- base_classes/tree.py
+++ base_classes/tree.py
@@ -46,30 +46,77 @@ class BinaryTree(object):
         """converte node to string"""
         if self.left:
             l = str(self.left.data)
+        else:
+            l = ""
         if self.right:
             r = str( self.right.data )
+        else:
+            r = ''
         return "(" + str(self.data) + ", " + l + ", " + r + ")"
 
     def traversal_inorder( self ):
         if self.left:
-           yield self.left.traversal_inorder()
+           yield from self.left.traversal_inorder()
         yield self
         if self.right:
-            self.right.traversal_inorder()
+            yield from self.right.traversal_inorder()
         return
 
     def traversal_posorder( self ):
         if self.left:
-            self.left.traversal_inorder()
+            yield from self.left.traversal_posorder()
         if self.right:
-            self.right.traversal_inorder()
+            yield from self.right.traversal_posorder()
         yield self
         return
 
     def traversal_preorder( self ):
         yield self
         if self.left:
-            self.left.traversal_preorder()
+            yield from self.left.traversal_preorder()
         if self.right:
-            self.right.traversal_preorder()
+            yield from self.right.traversal_preorder()
         return
+
+    def levels(self):
+        """Conta profundidade máxima para ambos os lados"""
+        if self.right :
+            r = self.right.levels()
+        else:
+            r = 0
+        if self.left:
+            l = self.left.levels()
+        else:
+            l = 0
+        return 1 + max( l, r )
+
+    @property
+    def childnodes(self):
+        """Quantidade de nos filhos"""
+        ret = 0
+        if self.left:
+            ret += ( 1 + self.left.childnodes)
+        if self.right:
+            ret += ( 1 + self.right.childnodes)
+        return ret
+
+    @property
+    def leafs(self):
+        """Quantidade de folhas"""
+        if ( self.left is None) and ( self.right is None):
+            ret =  1
+        else:
+            ret = 0
+            if self.left:
+                ret += self.left.leafs
+            if self.right:
+                ret += self.right.leafs
+        return ret
+
+    def toPlainText(self, cols ):
+        """Gera texto para apresentação da árvore"""
+        line = str( self.data ) .center( cols )
+        if self.left:
+            linkLine = ('/' + '-'*(5)).center( cols // 2 )
+        ret =  line + "\n" + linkLine
+        return ret
--- mainTree20191018.py
+++ mainTree20191018.py
@@ -2,13 +2,18 @@ from base_classes.tree import BinaryTree
 
 def main():
     root = BinaryTree( 1, BinaryTree( 2 ), BinaryTree( 3 ) )
-
+    root.insert( 4 )
     node = root.findLinear( 1 )
 
-    print( node )
-
+    print( 'Enumeração - inorder')
     for n in node.traversal_inorder():
         print ( n )
+
+    print( 'Possui ', root.levels(), 'niveis')
+    print( 'Possui ', root.childnodes, 'filhos')
+    print( 'Possui ', root.leafs, 'folhas')
+
+    print( root.toPlainText(80))
 
 main()
 
